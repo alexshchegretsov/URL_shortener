ae47ad048bb1be65bfb8918443672ffaa0dfeff4
alex_shchegretsov <nydollz77@gmail.com>
2019-11-02 15:52:03 +0300

    Add gunicorn workers

--- src/gunicorn.py
+++ src/gunicorn.py
@@ -4,7 +4,7 @@ import os
 
 bind = '0.0.0.0:' + os.environ.get('PORT', '8001')
 max_requests = 1000
-workers = multiprocessing.cpu_count()
-env = {'DJANGO_SETTINGS_MODULE': '<project>.settings'}
+workers = (multiprocessing.cpu_count() * 2) + 1
+env = {'DJANGO_SETTINGS_MODULE': 'SHRTN_engine.settings'}
 reload = True
 
